de3493c597c52add7a80a3d32f73876f08dc52a0
bbbozhena <bbbozhenka@gmail.com>
2023-03-01 14:28:15 +0200

    feat: add store

---
+++ src/Store/index.ts
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import UserSlice from "./Slices/UserSlice";
+ export const store = configureStore({
+    reducer:{
+        user:UserSlice
+    }
+ }) 
