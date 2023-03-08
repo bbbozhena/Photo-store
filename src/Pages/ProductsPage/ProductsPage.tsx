ce08af63a2a2972e56f87bf7170dc638350951a6
bbbozhena <bbbozhenka@gmail.com>
2023-03-08 11:41:06 +0200

    style: card

--- src/Pages/ProductsPage/ProductsPage.tsx
+++ src/Pages/ProductsPage/ProductsPage.tsx
@@ -1,9 +1,63 @@
 import React from "react";
+import "./ProductsPage.css"
 export function Products() {
-    return (
-      <div>
-        Ok
+  return (
+    <div>
+      <header className="d-flex justify-content-center">
+        <p>People</p>
+        <p>Nature</p>
+      </header>
+      <div className="cards ">
+        <div className="card">
+          <img src="/img/bubble.png" width={300} height={300}></img>
+          <p>Forest</p>
+          <div className="card-buttons">
+            <button>Add</button>
+            <button>Like</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/bubble.png" width={300} height={300}></img>
+          <p>Forest</p>
+          <div className="card-buttons">
+            <button>Add</button>
+            <button>Like</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/bubble.png" width={300} height={300}></img>
+          <p>Forest</p>
+          <div className="card-buttons">
+            <button>Add</button>
+            <button>Like</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/bubble.png" width={300} height={300}></img>
+          <p>Forest</p>
+          <div className="card-buttons">
+            <button>Add</button>
+            <button>Like</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/bubble.png" width={300} height={300}></img>
+          <p>Forest</p>
+          <div className="card-buttons">
+            <button>Add</button>
+            <button>Like</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/bubble.png" width={300} height={300}></img>
+          <p>Forest</p>
+          <div className="card-buttons">
+            <button>Add</button>
+            <button>Like</button>
+          </div>
+        </div>
+        
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
