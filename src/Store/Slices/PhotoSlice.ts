e4a6d9e2998050cf84144c752f9fd817d3eab667
bbbozhena <bbbozhenka@gmail.com>
2023-04-03 21:42:42 +0300

    build: create asyncThunk for photos

--- src/Store/Slices/PhotoSlice.ts
+++ src/Store/Slices/PhotoSlice.ts
@@ -1,17 +1,52 @@
-// import React from "react";
-// import { createSlice } from "@reduxjs/toolkit";
-// import { Router } from "express";
-// import axios from "axios";
-// import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-// export const fetchProducts = createAsyncThunk(
-//     "products/fetchProducts", async (_, thunkAPI) => {
-//        try {
-//           //const response = await fetch(`url`); //where you want to fetch data
-//           //Your Axios code part.
-//           const response = await axios.get(`url`);//where you want to fetch data
-//           return await response.json();
-//         } catch (error) {
-//            return thunkAPI.rejectWithValue({ error: error.message });
-//         }
-//   });
+type Photo = {
+  title: string;
+  src: string;
+  add: boolean;
+  like: boolean;
+  type: string;
+};
+
+type PhotoState = {
+  list: Photo[];
+  loading: boolean;
+  error: string | null;
+};
+
+export const fetchPhotos = createAsyncThunk<
+  Photo[],
+  undefined,
+  { rejectValue: string }
+>("photos/fetchPhotos", async function () {
+  const response = await fetch("http://localhost:3000/Photos");
+
+  const data = await response.json();
+  return data;
+});
+
+const initialState: PhotoState = {
+  list: [],
+  loading: false,
+  error: null,
+};
+
+const photoSlice = createSlice({
+  name: "photos",
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchPhotos.fulfilled, (state, action) => {
+        state.list = action.payload;
+        state.loading = false;
+      });
+  },
+});
+
+export default photoSlice.reducer;
